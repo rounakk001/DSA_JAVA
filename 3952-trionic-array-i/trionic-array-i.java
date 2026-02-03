8198cd8a173bb1b685180f44e303c28a60e393fc
Rounak singh <150698038+rounakk001@users.noreply.github.com>
2026-02-03 22:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (31.09%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -10,13 +10,16 @@ class Solution {
            
         }
        
-         if(n==nums.length-1) return false;
+         if(n==nums.length-1)  //n=nums.length-1 matlab ki n ki value change hi nahi hui jise pata lagta hai ki decresing part present hi nahi hai
+         return false;
         
         while(n>=1 && nums[n]<nums[n-1]){
             n--;
             
         }
-        if(n == 0) return false;
+
+        if(n == 0)  // iska matlab ki 
+        return false;
 
         
        
